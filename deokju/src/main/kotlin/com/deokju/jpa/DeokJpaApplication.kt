3daef42a0731c6daa748c1c12c0e7a3a3492e8fb
deokju <darkduk@naver.com>
2021-12-01 20:31:19 +0900

    프로젝트 초기셋팅 완료

---
+++ deokju/src/main/kotlin/com/deokju/jpa/DeokJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.deokju.jpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeokJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeokJpaApplication>(*args)
+}
